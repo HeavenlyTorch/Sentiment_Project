0095e7529a7f3e3085ee454554612db60c24a3d5
TheBrownishBox <aty4b01@gmail.com>
2024-04-23 13:32:42 +0400

    initial commit

--- audio_sentiment.py
+++ audio_sentiment.py
@@ -58,10 +58,12 @@ def setup_webrtc():
     webrtc_ctx = webrtc_streamer(
         key="audio_processor",
         mode=WebRtcMode.SENDRECV,
-        audio_processor_factory=lambda: AudioProcessor("wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000", aai.settings.api_key),
+        audio_processor_factory=lambda: AudioProcessor("wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000",
+                                                       aai.settings.api_key),
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         media_stream_constraints={"video": False, "audio": True}
     )
+
     return webrtc_ctx
 
 
