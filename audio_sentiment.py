ebef2a9623811c422badc1552d74cba6ab4a96f9
TheBrownishBox <aty4b01@gmail.com>
2024-04-24 09:41:41 +0400

    initial commit

--- audio_sentiment.py
+++ audio_sentiment.py
@@ -3,6 +3,7 @@ import base64
 import streamlit as st
 from google.cloud import speech, language_v1
 import io
+import wave
 
 # Set up Google Cloud API clients
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'trusty-sentinel-421215-f5581358b4be.json'
@@ -17,14 +18,23 @@ def analyze_sentiment(text):
 
 
 def transcribe_audio(audio_file):
+    # Read and close the WAV file
+    with wave.open(audio_file, 'rb') as wf:
+        sample_rate = wf.getframerate()  # Get the sample rate from the WAV file header
+
+    # Rewind the file pointer to the beginning of the audio file after reading the sample rate
+    audio_file.seek(0)
     audio_content = audio_file.read()
+
+    # Prepare the audio data for the Google API
     audio = speech.RecognitionAudio(content=audio_content)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,  # Make sure this matches the recording rate of the audio
+        sample_rate_hertz=sample_rate,  # Use the actual sample rate of the audio file
         language_code='en-US'
     )
 
+    # Perform the speech-to-text operation
     response = speech_client.recognize(config=config, audio=audio)
     transcript = " ".join([result.alternatives[0].transcript for result in response.results])
     return transcript
