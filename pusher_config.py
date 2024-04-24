b1864f339d918d4b9559086e6223690ae2071c33
TheBrownishBox <aty4b01@gmail.com>
2024-04-24 11:46:01 +0400

    initial commit

--- pusher_config.py
+++ pusher_config.py
@@ -2,8 +2,8 @@ from pusher import Pusher
 
 # Initialize Pusher client
 pusher_client = Pusher(
-    app_id='your_app_id',
-    key='your_app_key',
+    app_id='d8e7f8f7-23e3-4425-b037-29c66b684d99',
+    key='FFF21819C019D9082ED407D94789D2B91820B2EEEB13571481502785C1259E2E',
     secret='your_app_secret',
     cluster='your_app_cluster',
     ssl=True
