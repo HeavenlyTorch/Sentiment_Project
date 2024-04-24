79634a210c986a89de97c932b0a559317a3a0644
TheBrownishBox <aty4b01@gmail.com>
2024-04-24 09:33:19 +0400

    initial commit

--- audio_sentiment.py
+++ audio_sentiment.py
@@ -5,11 +5,18 @@ import matplotlib.pyplot as plt
 from google.cloud import speech, language_v1
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, AudioProcessorBase
 import logging
+from aioice import ice
 
 
-def show_audio_sentiment():
+async def show_audio_sentiment():
     st.title("WebRTC Test")
-    webrtc_streamer(key="test", mode=WebRtcMode.SENDRECV)
+    connection = ice.Connection(ice_controlling=True)
+    try:
+        await connection.gather_candidates()
+    except Exception as e:
+        print(f"Error gathering ICE candidates: {e}")
+    finally:
+        await connection.close()
 
 
 if __name__ == '__main__':
